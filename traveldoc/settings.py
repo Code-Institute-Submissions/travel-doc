7bbbe8ff4c73bf11de963da5a02c9f9414492961
Angela Anjorin <angela.anjorin@gmail.com>
2024-08-05 21:43:03 +0000

    Debug settings to false before deplyoment

--- traveldoc/settings.py
+++ traveldoc/settings.py
@@ -31,8 +31,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
+DEBUG = False
 ALLOWED_HOSTS = [
     '8000-angelaanjorin-traveldoc-zctotk6ntvs.ws-eu115.gitpod.io',
     '.herokuapp.com',
