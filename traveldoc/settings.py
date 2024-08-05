8d067d0c72f2dbb2167eee9d14a78dc5705fdaf5
Angela Anjorin <angela.anjorin@gmail.com>
2024-08-05 18:04:35 +0000

    Create an env.py file witht the secret key, the database urls and update the settings.py file

--- traveldoc/settings.py
+++ traveldoc/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-xrs%qkbgfhb%6%f9rlvm)_mob!v-5ebqvts*1jj=l4uhagznux'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
@@ -78,11 +82,16 @@ WSGI_APPLICATION = 'traveldoc.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
+#DATABASES = {
+ #   'default': {
+     #   'ENGINE': 'django.db.backends.sqlite3',
+     #   'NAME': BASE_DIR / 'db.sqlite3',
+    #}
+#}
+
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
 }
 
 
