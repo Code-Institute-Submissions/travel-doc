9badbeb8278721dfc1310a0f37d8de5b336975bd
Angela Anjorin <angela.anjorin@gmail.com>
2024-08-10 20:19:18 +0000

    git add --all git commit -m "create the addpost feature" -m "enable serving of image files" git push origin main

--- traveldoc/settings.py
+++ traveldoc/settings.py
@@ -55,6 +55,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cloudinary_storage',
     'django.contrib.sites',
     'allauth',
     'allauth.account',
@@ -62,6 +63,7 @@ INSTALLED_APPS = [
     'crispy_forms',
     'crispy_bootstrap5',
     'django_summernote',
+    'cloudinary',
     # Local
     'about',
     'accounts.apps.AccountsConfig',
