6c9c111dd4135e19437503be790ed6b5e81acbad
Angela Anjorin <angela.anjorin@gmail.com>
2024-08-18 19:11:30 +0000

    Chang ethe code so that the comments can be published immedialtly without approval from the admin. Debug the code so that all the comments are visible after submitting a comment.

--- blog/views.py
+++ blog/views.py
@@ -80,7 +80,7 @@ class post_detail(View):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
         comments = post.comments.all().order_by("-created_on")
-        comment_count = post.comments.filter(approved=True).count()
+        comment_count = post.comments.all().count()
         liked = False
         if post.likes.filter(id=self.request.user.id).exists():
             liked = True
@@ -100,27 +100,32 @@ class post_detail(View):
     def post(self, request, slug, *args, **kwargs):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(queryset, slug=slug)
-        comments = post.comments.filter(approved=True).order_by("-created_on")
-        comment_count = post.comments.filter(approved=True).count()
-        liked = False
-        if post.likes.filter(id=self.request.user.id).exists():
-            liked = True
+
 
         comment_form = CommentForm(data=request.POST)
         if comment_form.is_valid():
             comment = comment_form.save(commit=False)
             comment.author = request.user
             comment.post = post
-            comment.save() #Saves and publishes comment
+            comment.save() 
             messages.add_message(
                 request, messages.SUCCESS,
                 'Your comment has been posted and you can edit or delete it!'
             )
-            comment_form = CommentForm() #Resets the form after comment submission
+            #Resets the form after comment submission
+            comment_form = CommentForm() 
         else:
             messages.add_message(
                 request, messages.ERROR, "Error posting your comment."
             )
+
+        #Re-fetch the comments and comment count after the new comment is posted
+        comments = post.comments.all().order_by("-created_on")
+        comment_count = comments.count()
+        liked = False
+        if post.likes.filter(id=self.request.user.id).exists():
+            liked = True
+
 
         return render(
             request,
